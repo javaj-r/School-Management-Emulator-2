8ffa6616216f90b832b3f8d4738b4707ba4ac4a3
javid <javid.rafiee@gmail.com>
2022-03-09 23:29:07 +0330

    Refactor CrudRepository.

--- src/main/java/org/javid/repository/base/CrudRepository.java
+++ src/main/java/org/javid/repository/base/CrudRepository.java
@@ -2,19 +2,35 @@ package org.javid.repository.base;
 
 import org.javid.model.base.BaseEntity;
 
+import javax.persistence.EntityManager;
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * @author javid
  * Created on 1/30/2022
  */
-public interface CrudRepository<T  extends BaseEntity<ID>, ID> {
+public interface CrudRepository<T extends BaseEntity<ID>, ID extends Serializable> {
+
+    EntityManager getManager();
+
+    default void transaction(EntityManager manager, Runnable runnable) {
+        var t = manager.getTransaction();
+        try {
+            t.begin();
+            runnable.run();
+            t.commit();
+        } catch (Exception e) {
+            t.rollback();
+            e.printStackTrace();
+        }
+    }
 
     List<T> findAll();
 
     T findById(ID id);
 
-    ID save(T entity);
+    void save(T entity);
 
     void update(T entity);
 
