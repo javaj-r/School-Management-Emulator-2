c049c743ffbad365c6c1f80b4924cd799d746bcc
javid <javid.rafiee@gmail.com>
2022-03-09 23:30:24 +0330

    Refactor repositories.

--- src/main/java/org/javid/repository/CourseRepository.java
+++ src/main/java/org/javid/repository/CourseRepository.java
@@ -1,13 +1,7 @@
 package org.javid.repository;
 
 import org.javid.model.Course;
-import org.javid.model.Professor;
-import org.javid.model.Student;
 import org.javid.repository.base.CrudRepository;
 
 public interface CourseRepository extends CrudRepository<Course, Integer> {
-
-    void findAllCoursesByStudentId(Student student);
-
-    void findAllCoursesByProfessorId(Professor professor);
 }
