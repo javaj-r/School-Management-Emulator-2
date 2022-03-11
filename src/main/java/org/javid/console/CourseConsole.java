d77d0158e98199d29ef1107904aa6c8db8648a7d
javid <javid.rafiee@gmail.com>
2022-03-12 02:38:06 +0330

    Refactor CourseConsole.

--- src/main/java/org/javid/console/CourseConsole.java
+++ src/main/java/org/javid/console/CourseConsole.java
@@ -3,9 +3,11 @@ package org.javid.console;
 import org.javid.Application;
 import org.javid.console.base.BaseConsole;
 import org.javid.model.Course;
+import org.javid.model.base.BaseEntity;
 import org.javid.service.CourseService;
 import org.javid.util.Screen;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class CourseConsole extends BaseConsole<Course, Integer, CourseService> {
@@ -56,6 +58,7 @@ public class CourseConsole extends BaseConsole<Course, Integer, CourseService> {
     public void showAll() {
         service.findAll()
                 .stream()
+                .sorted(Comparator.comparing(Course::getId))
                 .map(Course::toString)
                 .forEach(System.out::println);
     }
@@ -84,10 +87,11 @@ public class CourseConsole extends BaseConsole<Course, Integer, CourseService> {
         if (Application.confirmMenu("Want to change required course: ") > 0) {
             course.setRequiredCourse(select("Select new required course: "));
         }
+        update(course);
+        System.out.println("Course updated successfully.");
+    }
 
-        if (Application.confirmMenu("Save changes") > 0) {
-            service.update(course);
-            System.out.println("Course updated successfully.");
-        }
+    public void update(Course course) {
+        service.update(course);
     }
 }
