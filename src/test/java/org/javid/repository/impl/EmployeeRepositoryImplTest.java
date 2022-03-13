0aafc1fda45b2a7e4cf9abf7816c47d60560b690
javid <javid.rafiee@gmail.com>
2022-03-13 14:48:47 +0330

    Refactor EmployeeRepositoryImplTest.

--- src/test/java/org/javid/repository/impl/EmployeeRepositoryImplTest.java
+++ src/test/java/org/javid/repository/impl/EmployeeRepositoryImplTest.java
@@ -12,11 +12,51 @@ class EmployeeRepositoryImplTest extends UserRepositoryImplTest<Employee, Employ
 
     @BeforeEach
     void setUp() {
-        System.out.println("setUp");
+        repository = new EmployeeRepositoryImpl(factory, Employee.class);
+        entity1 = new Employee()
+                .setUsername("e1")
+                .setPassword("123")
+                .setFirstname("fName1")
+                .setLastname("lName1")
+                .setNationalCode(123L)
+                .setSalary(10_000_000L);
+
+        entity2 = new Employee()
+                .setUsername("e2")
+                .setPassword("234")
+                .setFirstname("fName2")
+                .setLastname("lName2")
+                .setNationalCode(234L)
+                .setSalary(20_000_000L);
     }
 
     @AfterEach
     void tearDown() {
-        System.out.println("tearDown");
+        var em = repository.getManager();
+        repository.transaction(em, () -> em.createQuery("DELETE FROM Employee ")
+                        .executeUpdate());
+    }
+
+    @Override
+    protected void update() {
+        repository.save(entity1);
+        var id = entity1.getId();
+        entity1.setPassword(entity2.getPassword())
+                .setFirstname(entity2.getFirstname())
+                .setLastname(entity2.getLastname())
+                .setNationalCode(entity2.getNationalCode())
+                .setSalary(entity2.getSalary());
+
+        repository.update(entity1);
+
+        var actual = repository.findById(id);
+
+        assertAll(
+                () -> assertEquals(entity2.getPassword(), actual.getPassword()),
+                () -> assertEquals(entity2.getFirstname(), actual.getFirstname()),
+                () -> assertEquals(entity2.getLastname(), actual.getLastname()),
+                () -> assertEquals(entity2.getNationalCode(), actual.getNationalCode()),
+                () -> assertEquals(entity2.getSalary(), actual.getSalary())
+        );
     }
 }
