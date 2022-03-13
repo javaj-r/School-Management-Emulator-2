20a119e0fe7e4baa88dd4ecd74c1bc89a72016d5
javid <javid.rafiee@gmail.com>
2022-03-13 14:48:34 +0330

    Refactor UserRepositoryImplTest.

--- src/test/java/org/javid/repository/base/UserRepositoryImplTest.java
+++ src/test/java/org/javid/repository/base/UserRepositoryImplTest.java
@@ -3,25 +3,39 @@ package org.javid.repository.base;
 import org.javid.model.base.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled
 public abstract class UserRepositoryImplTest<T extends User, R extends UserRepository<T> & CrudRepository<T, Integer>> extends CrudRepositoryImplTest<T, Integer, R> {
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
+    /**
+     * Not for executing from hear
+     */
     @Test
     void findByUsername() {
+        repository.transaction(repository.getManager(), ()->repository.save(entity1));
+
+        var notSaved = repository.findByUsername(entity2);
+        var saved = repository.findByUsername(entity1);
+
+        assertNull(notSaved);
+        assertNotNull(saved);
     }
 
+    /**
+     * Not for executing from hear
+     */
     @Test
     void findByUsernameAndPassword() {
+        repository.transaction(repository.getManager(), ()->repository.save(entity1));
+
+        var notSaved = repository.findByUsername(entity2);
+        var saved = repository.findByUsername(entity1);
+
+        assertNull(notSaved);
+        assertNotNull(saved);
     }
 }
